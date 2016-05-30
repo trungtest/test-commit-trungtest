b1fd4beb68ac615b17e3a1c918f17175fe352045
trungtest <trungluongquang@phatmedia.vn>
2016-05-30 05:10:22 +0000

    additional.js file test
    test

---
+++ additional.js
@@ -0,0 +1,28 @@
+/*
+  This file is part of MyConference.
+
+  MyConference is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License version 3
+as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+  You should find a copy of the GNU Affero General Public License in the
+root directory along with this program.
+  If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+  */
+
+/*
+function to change Add New Agenda button in event.html
+ */
+
+function changeAddAgenda(addNewButton){
+  if(addNewButton.value === "Add New Agenda"){
+    addNewButton.value = "Hide";
+  }else{
+    addNewButton.value = "Add New Agenda";
+  }
+}
